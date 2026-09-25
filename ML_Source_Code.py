70dd8fe77cc2a6702be57344625bf43cd4cedd70
Aaryan Rajora <aryan.rajora14@outlook.com>
2026-09-25 14:49:50 +0530

    Remove duplicate matplotlib import
    Removed duplicate import of matplotlib.pyplot.

--- ML_Source_Code.py
+++ ML_Source_Code.py
@@ -7,7 +7,6 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 import seaborn as sns
 import joblib
-import matplotlib.pyplot as plt 
 import warnings
 warnings.filterwarnings('ignore')
 
